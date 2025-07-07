1df9ff15db6ebead9349cca10fcbcbd9d7cb5e5f
SokanRed <deltapi@sokan.fr>
2025-07-07 12:49:39 +0200

    Complete W02 Learning Activity: Document Object Model

---
+++ week02/scripts/bom.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+const li = document.createElement('li');
+//Create a delete button.
+const deleteButton = document.createElement('button');
+//Populate the li element variable's textContent or innerHTML with the input value.
+li.textContent = input.value;
+//Populate the button textContent with a ❌
+deleteButton.textContent = '❌';
+//Append the li element variable with the delete button.
+li.append(deleteButton);
+//Append the li element variable to the unordered list in your HTML.
+list.append(li);
